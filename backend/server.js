c4f6d406f601932337dba73155c1f93bba2ebc6d
suhasledange <suhasledange10@gmail.com>
2025-07-16 14:48:21 +0530

    backend setup

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import "dotenv/config"
+import cors from "cors"
+import http from "http"
+
+const app = express()
+const server = http.createServer(app)
+const PORT = process.env.PORT || 5000
+
+// middleware setup
+
+app.use(express.json({limit:"4mb"}))
+app.use(cors())
+
+app.use("/api/status",(req,res)=> res.send("Server is live"))
+
+
+server.listen(PORT,()=>{
+    console.log("Server is running on port "+PORT)
+})
